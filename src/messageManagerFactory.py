aec7bab6bf77a6c02acd4adc5fb0c833ad68c0f7
nik1168 <ngeisser32@gmail.com>
2020-05-04 23:50:51 +0200

    Comment code message manager factory

--- src/messageManagerFactory.py
+++ src/messageManagerFactory.py
@@ -7,6 +7,11 @@ class MessageManagerFactory:
         self.message_managers = []
 
     def create(self, max_input_size, topic_name):
+        """
+        Adds a message manager object that will handle the streaming for a given topic.
+        :param max_input_size: Size of the array that will be streamed
+        :param topic_name: Name of the topic to which the data will ben sent
+        """
         message_manager = MessageManager(max_input_size, topic_name)
         self.message_managers.append(message_manager)
 
@@ -14,5 +19,9 @@ class MessageManagerFactory:
         return self.message_managers
 
     def add_input(self, x):
+        """
+        Adds an element to every message manager currently defined.
+        :param x: Element to be added
+        """
         for message_manager in self.message_managers:
             message_manager.message.x.append(x)
